d5dc0ecc752da7a7e15919ab5093c54b5dc15975
Evandro oliveira <vandoaparecido@hotmail.com>
2022-11-01 09:11:27 -0300

    :soccer: web - Exibindo contagem com getServerSideProps

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+interface HomeProps{
+  count: number
+}
+
+export default function Home(props:HomeProps) {
+  // fetch('http://localhost:3333/pools/count')
+  // .then(response => response.json())
+  // .then(data => {
+  //   console.log(data)
+  // })
   return (
-    <h1>Hello world</h1>
+    <h1>Contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+ const response = await fetch('http://localhost:3333/pools/count')
+ const data = await response.json()
+ console.log(data)
+ return{
+  props: {
+    count:data.count,
+  }
+ }
 }
