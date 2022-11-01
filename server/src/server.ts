728006e40a81010b42562e91484081136d647cc0
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-31 23:05:01 -0300

    :soccer:  Retornando boloês pela primeira letra do campo código

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,26 @@
 import Fastify from 'fastify'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger:true,
     })
 
-    fastify.get('/pools/count', () =>{
-        return {count:0}
+    fastify.get('/pools/count', async () =>{
+
+        const pools = await prisma.pool.findMany({
+            where:{
+                code:{
+                    startsWith:'c'
+                }
+            }
+        })
+
+        return {pools}
     })
 
     await fastify.listen({port:3333})
